290c2c8c7baabd22f48c86330af8ac6ac863c104
sofonias gashaw dubale <48043913+sofoniasgd@users.noreply.github.com>
2023-09-16 03:14:16 +0300

    Create read.c

---
+++ read.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stddef.h>
+/**
+ * main - uses read() to get string from stdin
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+  char* buff[1024];
+int i;
+  ssize_t len, nbyte;
+  printf("$ ");
+  len = read(FILENO_STDIN, buff, size_t nbyte);
+return (0);
+}
